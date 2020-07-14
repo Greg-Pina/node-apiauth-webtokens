de25bf31817288e5d6a33d583b0208c3508eb11a
Greg Pina <pinagm2825@gmail.com>
2020-07-14 00:06:30 -0400

    installing package to hash user passwords

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,16 @@ const User = require('../model/User')
 const { registerValidation } = require('../validation')
 
 router.post('/register', async (req, res) => {
+	// Validate we get from user
 	const { error } = registerValidation(req.body)
 
 	if (error) return res.status(400).send(error.details[0].message)
 
+	// check if user already exists in db
+	const emailExist = await User.findOne({ email: req.body.email })
+	if (emailExist) return res.status(400).send('Email already exists')
+
+	// Create a new user
 	const user = new User({
 		name: req.body.name,
 		email: req.body.email,

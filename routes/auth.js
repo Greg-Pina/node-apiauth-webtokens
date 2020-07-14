ba79fc186e9c9617d853dad0c487fb0b1e017483
Greg Pina <pinagm2825@gmail.com>
2020-07-13 23:32:49 -0400

    Joi error catching and validating user data

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,23 @@
 const router = require('express').Router()
 const userModel = require('../model/User')
 
+//Validation
+const Joi = require('@hapi/joi')
+const { Error } = require('mongoose')
+const { ValidationError } = require('@hapi/joi')
+
+const schema = Joi.object({
+	name: Joi.string().min(6).required(),
+	email: Joi.string().min(6).required().email(),
+	password: Joi.string().min(6).required().email(),
+})
 router.post('/register', async (req, res) => {
+	// Validate data before new user entry
+	try {
+		const error = Joi.assert(req.body, schema)
+	} catch (err) {
+		res.status(400).send(err.details[0].message)
+	}
 	const user = new userModel({
 		name: req.body.name,
 		email: req.body.email,

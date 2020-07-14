31839a5a40b5a388d8910b2821da2d1e2f714459
Greg Pina <pinagm2825@gmail.com>
2020-07-13 22:56:27 -0400

    updated node modules and created userModel to hit DB

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
-//Import Routes
+// Load config
+dotenv.config({ path: './config/config.env' })
+
+// Connect to DB
+connectDB()
+
+// Import Routes
 const authRoute = require('./routes/auth')
+
+// Middleware
+app.use(express.json())
 
 // Route Middleware
 app.use('/api/user', authRoute)
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,20 @@
 const router = require('express').Router()
+const userModel = require('../model/User')
 
-router.post('/register', (req, res) => {
-	res.send('Register')
+router.post('/register', async (req, res) => {
+	const user = new userModel({
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password,
+		date: req.body.date,
+	})
+
+	try {
+		const savedUser = await user.save()
+		res.send(savedUser)
+	} catch (err) {
+		res.status(400).send(err)
+	}
 })
 
-module.exports = router()
+module.exports = router

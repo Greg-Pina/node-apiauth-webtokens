22626d8ca99badfaa9bfa7bb487df8b8ff63a5b0
Greg Pina <pinagm2825@gmail.com>
2020-07-14 00:02:41 -0400

    weird Joi method calls, fixed

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,16 @@
 const router = require('express').Router()
-const userModel = require('../model/User')
+const User = require('../model/User')
+const { registerValidation } = require('../validation')
 
-//Validation
-const Joi = require('@hapi/joi')
-const { Error } = require('mongoose')
-const { ValidationError } = require('@hapi/joi')
-
-const schema = Joi.object({
-	name: Joi.string().min(6).required(),
-	email: Joi.string().min(6).required().email(),
-	password: Joi.string().min(6).required().email(),
-})
 router.post('/register', async (req, res) => {
-	// Validate data before new user entry
-	try {
-		const error = Joi.assert(req.body, schema)
-	} catch (err) {
-		res.status(400).send(err.details[0].message)
-	}
-	const user = new userModel({
+	const { error } = registerValidation(req.body)
+
+	if (error) return res.status(400).send(error.details[0].message)
+
+	const user = new User({
 		name: req.body.name,
 		email: req.body.email,
 		password: req.body.password,
-		date: req.body.date,
 	})
 
 	try {
